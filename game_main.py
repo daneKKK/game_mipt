b355f0efaab8c82e1c16e7ebf946a8e1ac70423a
unknown <daniaalekseew@yandex.ru>
2021-12-04 19:52:47 +0300

    Второй набросок game_main.py

--- game_main.py
+++ game_main.py
@@ -1,22 +1,28 @@
 import pygame as pg
 import numpy as np
+import os, random
 
 from game_objects import *
 from game_input import *
 from game_vis import *
 
+#Переменные для отсчёта времени
 FPS = 30
 clock = pg.time.Clock()
 timer = 0
 
+#Данные по игре
 levels = []
 player = []
 current_level_index = 0
+isOpened = False
+
+#Жива ли программа
 alive = True
-isOpened
 
 
 def load_save(filename):
+    #Загрузка сейва
     global levels
     global player
     global current_level_index
@@ -26,19 +32,42 @@ def load_save(filename):
         print('Файл не найден!')
 
 def checkPlayerOnLevel():
-    pass
+    '''Проверка местонахождения игрока на уровне
+    '''
+    global player
+    global current_level_index
+    global levels
+    if player.x >= 9.5 and player.x <= 10.5 and player.y >= 19:
+        current_level_index += 1
+    if player.x >= 9.5 and player.x <= 10.5 and player.y <= 1:
+        if current_level_index > 0:
+            current_level_index -= 1
+    if current_level_index + 1 > len(levels):
+        createNewLevel()
+
+    
 
 def createNewLevel():
+    '''
+    Создание нового уровня из библиотеки уровней
+    '''
     global levels
-    new_level = []
+    new_level_name = random.choice(os.listdir("levels\\"))
+    new_level = read_level_objects_data(new_level_name)
     levels += [new_level]
 
 def setPlayer():
+    '''
+    Создание нового игрока
+    '''
     global player
 
     player = Player()
 
 def entity_ai(entity, level, player):
+    '''
+    Обработка действий мобов
+    '''
     global timer
     pass
 
@@ -54,6 +83,7 @@ def main():
     width = 800
     height = 800
     screen = pg.display.set_mode((width, height))
+    drawer = Drawer(screen)
 
     while alive:
         screen.fill((255, 255, 255))
@@ -73,7 +103,7 @@ def main():
                 anyEnemyLeft = True
             draw(i, screen)
 
-        draw(player, screen)
+        drawer.update(levels[current_level_index], player)
 
 
 
