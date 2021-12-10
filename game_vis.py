23d196ffb3e13fc3d8f54d7085e1db1dc0566a4b
Kita Romashkin <N.10.10.1@mail.ru>
2021-12-10 21:56:55 +0300

    add texture for entities

--- game_vis.py
+++ game_vis.py
@@ -39,9 +39,22 @@ class Drawer:
         elif obj.type == "arrow":
             self.draw_arrow(obj)
 
+    def rot_center(image, angle):
+
+        """rotate an image while keeping its center and size"""
+
+        orig_rect = image.get_rect()
+        rot_image = pg.transform.rotate(image, angle)
+        rot_rect = orig_rect.copy()
+        rot_rect.center = rot_image.get_rect().center
+        rot_image = rot_image.subsurface(rot_rect).copy()
+        return rot_image
+
     def draw_entity(self, obj):
-        pg.draw.circle(self.screen, (0, 0, 0),
-                       (scale_x(obj.x), scale_y(obj.y)), scale_size(obj.r))
+        texture_surface = pg.image.load(obj.texturepath)
+        texture_surface = pg.transform.scale(texture_surface, (scale_size(obj.r), (scale_size(obj.r))))
+        texture_surface = self.rot_center(texture_surface, obj.facing_angle)
+        self.screen.blit(texture_surface, (scale_x(obj.x - obj.r), scale_y(obj.y - obj.r)))
 
     def draw_wall(self, obj):
         pg.draw.rect(self.screen, (0, 0, 0),
