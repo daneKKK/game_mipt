fc42ba332584e697c101e440184d46f9e09c29ad
Nikitushechka <khomiakov.na@phystech.edu>
2021-12-11 17:44:21 +0300

    Сделал сохранения и загрузку и задокументировал функцию

--- game_input.py
+++ game_input.py
@@ -4,57 +4,57 @@ from random import randint as rnd
 
 from game_objects import *
 
+
 def read(filename):
+    '''
+    Читает json файл и возвращает информацию из него
+    '''
     with open(filename, 'r') as file:
         return json.load(file)
 
 
 def write(data, filename):
+    '''
+    Записывает информацию в json файл
+    '''
     data = json.dumps(data)
     data = json.loads(str(data))
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4)
 
 
-def read_level_objects_data(filename):
-    '''
-    i_spider, i_skelet, i_wall отвечают за кол-во спавнящихся Entity min = 0 max = 4 or 3
-    Функция считывает файл формата json, ищет в нём пауков,
-    скелетов и стены, засовывает их в общий список объектов
-    '''
-
-    spider_amount = rnd(0, 4)
-    skelet_amount = rnd(0, 3)
-    wall_amount = rnd(0, 3)
-
-    i_spider = 0
-    i_skelet = 0
-    i_wall = 0
-    
+def read_new_level(filename):
+   '''
+   Берёт filename, достаёт из него информацию, прогоняет
+   её через read_level_objects_data и возвращает результат
+   '''
     level = read(filename)
+    return read_level_objects_data(level)
+
+
+def read_level_objects_data(data):
+    '''
+    Принимает массив уровня и возвращает уровень, как объект
+    '''
+    
+    level = data
     level_object = Level()
     level_object.obj_list = []
     for obj in level:
         if obj == 'spiders':
-            while i_spider < spider_amount:
-                spider =read_spider_data(level[obj], i_spider)
+            for i in range(len(level[obj])):
+                spider =read_spider_data(level[obj], i)
                 level_object.obj_list.append(spider)
-                i_spider += 1
         if obj == 'skelets':
-            while i_skelet < skelet_amount:
-                skelet =read_skelet_data(level[obj], i_skelet)
-                level_object.obj_list.append(skelet)
-                i_skelet += 1
+            for i in range(len(level[obj])):
+               skelet = read_skelet_data(level[obj], i)
+               level_object.obj_list.append(skelet)
         if obj == 'walls':
-            while i_wall < wall_amount:
-                wall = read_wall_data(level[obj], i_wall)
+            for i in range(len(level[obj])):
+                wall = read_wall_data(level[obj], i)
                 level_object.obj_list.append(wall)
-                i_wall += 1
 
     return level_object
-
-
-
 
 
 def read_spider_data(line, spider_number):
@@ -64,57 +64,52 @@ def read_spider_data(line, spider_number):
     Spider <x> <y>
     s - кол-во пауков в файле
     '''
-    
-    #spider.id = rnd(0, 15000)
     x = line[spider_number][0]
     y = line[spider_number][1]
     return Spider(x, y)
     
-
 
 def read_skelet_data(line, skelet_number):
     '''
     Skelet <x> <y>
     s - кол-во скелетов в файле
     '''
-    #skelet.id = rnd(0, 15000)
     x = line[skelet_number][0]
     y = line[skelet_number][1]
     return Skelet(x, y)
+
 
 def read_wall_data(line, wall_number):
     '''
     Wall <x> <y>
     s - кол-во стен в файле
     '''
-    #wall.id = rnd(0, 15000)
     x = line[wall_number][0]
     y = line[wall_number][1]
     return Wall(x, y)
 
 
-def save_data(levels, character, filename):
-    #saves/[name].json
-    #Level 1: object_list = [spider, wall, bomb]
-    #Level 2: list = ...
-    #...
-    #Character: [health, gold, weapon]
+def save_data(levels, character, current_level_index, filename):
+    '''
+    Сохраняет игру в json файл
+    '''
     data = [i.toJSON() for i in levels]
-    data += [character.toJSON()]
+    data += [character.toJSON(current_level_index)]
     write(data, filename)
 
 
 def load_data(filename):
-    #распакуй мне [name].json в переменную list_of_objects_and_character
-    #list_of_objects_and_character[последний индекс] = character
-    #return list...
+    '''
+    Выполняет загрузку levels и character из файла failname
+    '''
     data = read(filename)
-    levels = [data[i] for i in range(len(data)) if i < len(data) - 1]
-    character = data[len(data) - 1]
-    return levels, character
-
-
-
+    levels = [read_level_objects_data(data[i]) for i in range(len(data)) if i < len(data) - 1]
+    character = Player()
+    character.health = float(data[len(data)-1]['health'])
+    character.x = float(data[len(data)-1]['x'])
+    character.y = float(data[len(data)-1]['y'])
+    current_level_index = int(data[len(data)-1]['current_level'])
+    return levels, character, current_level_index
 
 #в мейне:
 #information = load_data(save.json)
