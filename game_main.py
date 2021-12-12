7d048fbe7ed3559d28ac1f3623432f962904f40a
unknown <daniaalekseew@yandex.ru>
2021-12-12 15:32:50 +0300

    Задокументирован game_main

--- game_main.py
+++ game_main.py
@@ -23,10 +23,10 @@ attackedAlready = False
 #Жива ли программа
 alive = True
 
-
-
-
 def saveGame(filename):
+    '''
+    Сохранение уровня, игрока, индекса текущего уровня в filename.json
+    '''
     global levels
     global player
     global current_player_index
@@ -36,6 +36,10 @@ def saveGame(filename):
 
 
 def loadGame(filename):
+    '''
+    Загрузка уровня, игрока, индекса текущего уровня из filename.json
+    Если файла не существует, то в консоли выведится ошибка.
+    '''
     global levels
     global player
     global current_player_index
@@ -47,6 +51,9 @@ def loadGame(filename):
         print('Файл не найден!')
 
 def endGame():
+    '''
+    Меню конца игры. Может заново запустить main()
+    '''
     global screen
     global current_level_index
     game_over_menu = pygame_menu.Menu('Конец игры', 800, 800,
@@ -61,7 +68,7 @@ def endGame():
 
 
 def checkPlayerOnLevel():
-    '''Проверка местонахождения игрока на уровне
+    '''Проверка местонахождения игрока в пределах уровня
     '''
     global player
     global current_level_index
@@ -85,6 +92,11 @@ def checkPlayerOnLevel():
         player.y = player.r
 
 def checkEntityInWalls(obj, level):
+    '''
+    Проверка, находится ли объект в obj в какой-либо из стен уровня level.
+    obj - объект класса Entity,
+    level - объект класса Level
+    '''
     points = [(obj.x - obj.r / (2)**(1/2), obj.y - obj.r / (2)**(1/2)),
               (obj.x + obj.r / (2)**(1/2), obj.y - obj.r / (2)**(1/2)),
               (obj.x - obj.r / (2)**(1/2), obj.y + obj.r / (2)**(1/2)),
@@ -130,21 +142,28 @@ def entity_ai():
     global player
     global levels
     global current_level_index
+    
     for i in levels[current_level_index].obj_list:
+        #Обработка живых существ на уровне (кроме игрока)
         if i.living:
+            #Проверка на смерть
             if i.health <= 0:
                 levels[current_level_index].obj_list.remove(i)
                 continue
+            #Проверка на видимость игрока
             if levels[current_level_index].line_of_sight((i.x, i.y),
                                                          (player.x, player.y)):
                 angle = math.atan2((player.y - i.y), (player.x - i.x))
+                #Движение к игроку при определённых условиях, поворот к игроку,
+                #смена анимации движения
                 if (not (i.type == "skelet" and
                          ((player.x - i.x) ** 2 + (player.y - i.y) ** 2 <= 9))
                     and not ((player.x - i.x) ** 2 + (player.y - i.y) ** 2 <= 1)):
-                 i.move(angle)
-                 if timer % 10 == 0:
-                     i.changeTexture("move")
+                    i.move(angle)
+                    if timer % 10 == 0:
+                        i.changeTexture("move")
                 i.look_at(angle)
+                #Атака при определённых условиях
                 if (((player.x - i.x) ** 2 + (player.y - i.y) ** 2 <= 2.25
                      or i.type == "skelet")
                     and timer % 30 == 0):
@@ -153,13 +172,19 @@ def entity_ai():
                                                 levels[current_level_index].obj_list,
                                                 player)
                     levels[current_level_index].obj_list = new_list
+            #Проверка на столкновение со стенами
             checkEntityInWalls(i, levels[current_level_index])
+
+        #Обработка поведения стрел: попадание в сущности, стены или границы уровня
         if i.type == "arrow":
             new_list, player = i.move(levels[current_level_index], player)
             levels[current_level_index].obj_list = new_list
             if i.x >= 20 or i.x <= 0 or i.y >= 20 or i.y <= 0:
                 levels[current_level_index].obj_list.remove(i)
 def mainMenu():
+    '''
+    Функция, создающая главное меню.
+    '''
     menu = pygame_menu.Menu('Главное меню', 800, 800,
                             theme=pygame_menu.themes.THEME_BLUE)
     menu.add.button('Играть', mainloop)
@@ -169,6 +194,9 @@ def mainMenu():
     return menu
 
 def saveMenu():
+    '''
+    Функция, создающая подменю сохранения игры
+    '''
     saveMenu = pygame_menu.Menu('Сохранение', 800, 800,
                                 theme=pygame_menu.themes.THEME_BLUE)
     saveMenu.add.label('Введите название сохранения', max_char=-1,
@@ -180,6 +208,9 @@ def saveMenu():
     return saveMenu
 
 def loadMenu():
+    '''
+    Функция, создающая подменю загрузки игры
+    '''
     loadMenu = pygame_menu.Menu('Загрузка', 800, 800,
                                 theme=pygame_menu.themes.THEME_BLUE)
     loadMenu.add.label('Введите название сохранения', max_char=-1,
@@ -192,6 +223,9 @@ def loadMenu():
 
 
 def main():
+    '''
+    Первый запуск главного меню
+    '''
     global levels
     global player
     global current_level_index
@@ -202,6 +236,16 @@ def main():
     global save_menu
     global load_menu
 
+    #Обнуление всех игровых переменных
+    levels = []
+    player = []
+    current_level_index = 0
+    timer = 0
+    screen = []
+    drawer = []
+    main_menu = []
+
+    #Инициализация pygame и окна
     pg.init()
 
     flags = DOUBLEBUF
@@ -212,12 +256,16 @@ def main():
 
     setPlayer()
 
+    #Создание менюшек и запуск главного меню
     save_menu = saveMenu()
     load_menu = loadMenu()
     main_menu = mainMenu()
     main_menu.mainloop(screen)
 
 def mainloop():
+    '''
+    Фунция основного цикла игры
+    '''
     global levels
     global player
     global current_level_index
@@ -228,32 +276,45 @@ def mainloop():
     global save_menu
     global load_menu
 
+    
     alive = True
+
+    #Двигался или атаковал ли игрок недавно
     hasMoved = False
     hasAttacked = False
 
+    #Основной цикл
     while alive:
+        #Обнуление замеченных врагов
         anyEnemyLeft = False
 
+        #Проверка на нахождение игрока в пределах уровня
+        checkPlayerOnLevel()
+
+        #Обнуление информации о движении и атаке
         hasMoved = False
         if timer % 15 == 0:
             hasAttacked = False
-        
-        checkPlayerOnLevel()
-        
+
+        #Обработка событий игрока
         for event in pg.event.get():
+            #Выход
             if event.type == pg.QUIT:
                 alive = False
                 pg.quit()
+            #Кнопки на клавиатуре
             elif event.type == pg.KEYDOWN:
+                #Выход в меню
                 if event.key == pg.K_ESCAPE:
                     alive = False
                     main_menu.mainloop(screen)
+                #Смена оружия
                 elif event.key == pg.K_f:
                     if player.weapon.type == "sword":
                         player.weapon = Bow()
                     else:
                         player.weapon = Sword()
+            #Атака, если игрок не атаковал недавно
             elif event.type == pg.MOUSEBUTTONDOWN and not attackedAlready:
                 attack_position = ((event.pos[0] - 20) / 760 * 20,
                                    (event.pos[1] - 20)/760 * 20)
@@ -269,11 +330,13 @@ def mainloop():
                     levels[current_level_index].obj_list = new_obj
                     hasAttacked = True
                     player.changeTexture("attack")
+            #Направление взгляда персонажа
             elif event.type == pg.MOUSEMOTION:
                 angle = math.atan2(((event.pos[1] - 20) / 760 * 20 - player.y),
                                    ((event.pos[0] - 20)/760 * 20 - player.x))
                 player.look_at(angle)
-                    
+
+        #Обработка движения персонажа
         if pg.key.get_pressed()[pg.K_s]:
             player.move(math.pi/2)
             hasMoved = True
@@ -287,27 +350,32 @@ def mainloop():
             player.move(math.pi)
             hasMoved = True
 
+        #Проверка, находится ли игрок в стене
         checkEntityInWalls(player, levels[current_level_index])
 
+        #Анимация движения
         if hasMoved and not hasAttacked and timer % 10 == 0:
             player.changeTexture("move")
 
-        for i in levels[current_level_index].obj_list:
-            if i.living:
-                anyEnemyLeft = True
-
+        #Проверка на наличие врагов на уровне
+        anyEnemyLeft = any([i.living for i in levels[current_level_index].obj_list])
+            
+        #Интеллект мобов
         entity_ai()
 
+        #Проверка персонажа на смерть
         if player.health <= 0:
             endGame()
-        
+
+        #Рисование на экране, если программа ещё работает
         if alive:
             drawer.update(levels[current_level_index], player,
                           current_level_index)
 
 
-
+        #Увеличение таймера (от него зависит скорость атаки всех сущностей)
         timer += 1
+        
         clock.tick(FPS)
 
     pg.quit()
